b17d16afef01060ee09fc840c5d5ddb2af22f37c
VenkateshVanaras <venkateshvanaras7@gmail.com>
2024-12-22 20:28:51 +0530

    hello tillu

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import Welcome  from './Welcome';
+import './App.css';
+
+function App() {
+  return (
+    <>
+   
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+
+      </header>
+    </div>
+    <Welcome />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Welcome/index.js
@@ -0,0 +1,10 @@
+const Welcome = () => {
+    return (
+        <div>
+            <h1> This pape is about react js </h1>
+            <p> Run the file if everything is fine. </p>
+        </div>
+    )
+}
+
+export default Welcome;
